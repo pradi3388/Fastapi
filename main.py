0ee34c8b64525df8c06f32fd268e0be6fbc58f7f
pradeep <pradi3388@gamil.com>
2022-05-29 08:45:24 +0530

    integrated Package

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
-import uvicorn
+from fastapi import HTTPException, Request, status, Depends
+from sqlalchemy.orm import Session
+import db, schema
+
+db.Base.metadata.create_all(bind = db.engine)
 
 app = FastAPI()
 
 
-@app.post("/predict")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/users/create", status_code=status.HTTP_201_CREATED, response_model=schema.userInput)
+def create(user: schema.userInput, dbd: Session = Depends(db.get_db)):
+    user_email = dbd.query(db.User).filter(db.User.email == user.email).first()
 
-@app.get("/predict")
-async def root():
-    return {"message": "Hello India"}
+    if not user_email:
+        user_dict = user.dict()
+        new_user = db.User(**user_dict)
+        dbd.add(new_user)
+        dbd.commit()
+        dbd.refresh(new_user)
+        
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail = f"Email id already exists"
+        )
+
+    return new_user
+
+if __name__ == '__main__':
+    app.run()
---
+++ schema.py
@@ -0,0 +1,13 @@
+from re import S
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+
+class userInput(BaseModel):
+    email: EmailStr
+    first_name: str
+    last_name: str
+    password: str
+
+    class Config:
+        orm_mode = True
